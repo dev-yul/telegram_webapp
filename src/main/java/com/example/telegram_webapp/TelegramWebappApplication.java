41e0732b8c1b5baf2a6d44ba9329cdbb7fc73c5c
dev-yul <yul.dev0206@gmail.com>
2025-01-27 09:27:16 +0900

    자바로 업로드

---
+++ src/main/java/com/example/telegram_webapp/TelegramWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegram_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramWebappApplication.class, args);
+	}
+
+}
